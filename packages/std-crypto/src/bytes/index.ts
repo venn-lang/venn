02ce5b9c090e14c25f238decc0870b92e6d54926
Vinicius Borges <viniciusborgeis@gmail.com>
2026-07-28 01:26:35 -0300

    feat(crypto): add byte and text encoding helpers

---
+++ packages/std-crypto/src/bytes/index.ts
@@ -0,0 +1,12 @@
+export type { Bytes } from "./bytes.js";
+export {
+  equals,
+  fromBase64,
+  fromBase64Url,
+  fromBytes,
+  fromHex,
+  toBase64,
+  toBase64Url,
+  toBytes,
+  toHex,
+} from "./bytes.js";
